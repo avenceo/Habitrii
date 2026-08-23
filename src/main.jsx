7e8f1940353de4bcfd3576551c110eadfeac2409
avenceo <ceo@aven4life.com>
2026-08-23 12:25:29 -0400

    Add Watermark component to main render

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Watermark from './Watermark.jsx'
 import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Watermark />
     <Analytics />
   </React.StrictMode>,
 )
